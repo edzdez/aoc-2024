3f7789715d82400e9e68c566be7556c838b5056a
Ethan Zhang <ethanz4@illinois.edu>
2024-12-01 20:14:51 -0800

    update day1p2 use of maybe

--- src/Day01.hs
+++ src/Day01.hs
@@ -3,7 +3,6 @@ module Day01 where
 import Data.List
 import Data.List.Split
 import Data.Map.Strict qualified as M
-import Data.Maybe
 
 parseInput :: String -> [[Int]]
 parseInput = transpose . map parseLine . lines
@@ -16,5 +15,5 @@ part1 = sum . map (abs . foldr (-) 0) . transpose . map sort
 part2 :: [[Int]] -> Int
 part2 [xs, ys] = sum $ map (\n -> n * M.findWithDefault 0 n freqs) xs
   where
-    freqs = foldr (M.alter (\v -> Just $ (1 :: Int) + fromMaybe 0 v)) M.empty ys
+    freqs = foldr (M.alter (Just . maybe 1 (+ 1))) M.empty ys
 part2 _ = undefined
