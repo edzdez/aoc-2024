1aa72173502f325fd84fc7aa78bb247d642be08d
Ethan Zhang <ethanz4@illinois.edu>
2024-12-01 21:32:56 -0800

    day 2

---
+++ src/Day02.hs
@@ -0,0 +1,28 @@
+module Day02 where
+
+import Data.List
+import Data.List.Split
+
+parseInput :: String -> [[Int]]
+parseInput = map (map read . splitOn " ") . lines
+
+validReport :: [Int] -> Bool
+validReport report = diffsWithin 1 3 pairs || diffsWithin (-3) (-1) pairs
+  where
+    pairs = zip report (tail report)
+
+diffsWithin :: Int -> Int -> [(Int, Int)] -> Bool
+diffsWithin lo hi = all (\(x, y) -> lo <= y - x && y - x <= hi)
+
+removeOne :: [Int] -> [[Int]]
+removeOne input = filter ((== n - 1) . length) $ subsequences input
+  where
+    n = length input
+
+part1 :: [[Int]] -> Int
+part1 = length . filter validReport
+
+part2 :: [[Int]] -> Int
+part2 = length . filter validWithRemoval
+  where
+    validWithRemoval report = validReport report || any validReport (removeOne report)
