f353a68773c48498bc97e7b0c4bc407df9dade37
Ethan Zhang <ethanz4@illinois.edu>
2024-12-03 23:19:18 -0800

    refactor day 4 to use Data.Matrix (it's now much faster!)

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,48 +1,54 @@
-module Day04 (parseInput, part1, part2) where
+module Day04 where
 
-import Data.List
 import Data.List.Split
-import Data.Maybe
+import Data.Matrix qualified as M
+import GHC.Base
 
-parseInput :: String -> [[Char]]
-parseInput = lines
+cartesianProduct :: [a] -> [b] -> [(a, b)]
+cartesianProduct xs ys = [(i, j) | i <- xs, j <- ys]
 
-countXmasInLine :: [Char] -> Int
+parseInput :: String -> M.Matrix Char
+parseInput = M.fromLists . lines
+
+countXmasInLine :: String -> Int
 countXmasInLine = length . filter (== "XMAS") . divvy 4 1
 
-(!?) :: [a] -> Int -> Maybe a
-[] !? _ = Nothing
-(x : xs) !? i
-  | i < 0 = Nothing
-  | i == 0 = Just x
-  | otherwise = xs !? (i - 1)
+takeDiagonal :: M.Matrix a -> Int -> [a]
+takeDiagonal mat i =
+  let n = M.nrows mat
+   in zipWith (\a b -> M.getElem a b mat) [1 .. n - i + 1] (enumFrom i)
 
-takeDiagonal :: [[Char]] -> Int -> [Char]
-takeDiagonal mat = map (fromMaybe '.') . takeWhile isJust . zipWith (!?) mat . enumFrom
-
-diagonals :: [[Char]] -> [[Char]]
+diagonals :: M.Matrix a -> [[a]]
 diagonals mat =
-  let n = length mat
-   in reverse (takeDiagonal' mat [0 .. n - 1]) ++ takeDiagonal' (transpose mat) [1 .. n - 1]
+  let n = M.nrows mat
+   in reverse (takeDiagonal' mat [1 .. n]) ++ takeDiagonal' (M.transpose mat) [2 .. n]
   where
     takeDiagonal' mat' = map (takeDiagonal mat')
 
-part1 :: [[Char]] -> Int
+part1 :: M.Matrix Char -> Int
 part1 board =
-  let verticalFlip = transpose board
-      southEast = diagonals board
-      southWest = diagonals $ map reverse board
-   in sum $ map ((+) <$> countXmas <*> countXmas') [board, verticalFlip, southEast, southWest]
+  let southEast = diagonals board
+      southWest = diagonals $ M.fromLists $ map reverse $ M.toLists board
+   in sum $
+        map
+          ((+) <$> countXmas id <*> countXmas reverse)
+          [ M.toLists board,
+            M.toLists $ M.transpose board,
+            southEast,
+            southWest
+          ]
   where
-    countXmas = sum . map countXmasInLine
-    countXmas' = sum . map (countXmasInLine . reverse)
+    countXmas f = sum . map (countXmasInLine . f)
 
-part2 :: [[Char]] -> Int
-part2 = length . concatMap (filter isXMas . divvy 3 1 . transpose) . divvy 3 1
+part2 :: M.Matrix Char -> Int
+part2 board =
+  length $
+    filter (isXMas . \(a, b) -> M.submatrix a (a + 2) b (b + 2) board) $
+      join cartesianProduct [1 .. M.nrows board - 2]
   where
     isXMas c =
-      let southEast = takeDiagonal c 0
-          southWest = takeDiagonal (map reverse c) 0
+      let southEast = takeDiagonal c 1
+          southWest = takeDiagonal (M.fromLists $ map reverse $ M.toLists c) 1
        in isMas southWest && isMas southEast
       where
         isMas = (||) <$> (== "MAS") <*> (== "SAM")
